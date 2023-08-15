5d124bb3f48d9b60996dd5ac63ff7e504a936b31
GeovanniVeloso <141738879+GeovanniVeloso@users.noreply.github.com>
2023-08-14 22:05:08 -0300

    Add files via upload

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,5 +1,18 @@
 package view;
 
-public class Principal {
+import controller.RecursivaPositivo;
 
+public class Principal {
+	public static void main(String[] args) {
+		RecursivaPositivo rp = new RecursivaPositivo();
+		
+		int VT = 5;
+		int soma = 0;
+		int[] vetor = {-7,4,-1,6,-9};
+		
+		int res = rp.positivo(soma, vetor, VT);
+		
+		System.out.println(res);
+
+	}
 }
